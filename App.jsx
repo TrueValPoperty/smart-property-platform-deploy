de8ec277e0deea966e81c50d7a679ec49edb40e3
TrueValPoperty <williamtylerstreet@gmail.com>
2025-07-01 22:00:41 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,26 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './dashboard';
-import Pricing from './pricing';
-import Login from './login';
-import Admin from './admin';
-import TrueVal from './trueval';
-import PlanScore from './planscore';
-import EcoStay from './ecostay';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/modules/trueval" element={<TrueVal />} />
-        <Route path="/modules/planscore" element={<PlanScore />} />
-        <Route path="/modules/ecostay" element={<EcoStay />} />
-      </Routes>
-    </Router>
-  );
+    <div className="text-center mt-20 text-2xl text-blue-600">
+      Smart Property Platform: Hello World!
+    </div>
+  )
 }
-
-export default App;
+export default App
